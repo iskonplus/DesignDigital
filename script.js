acb2c80f42aecc5701cc9b742cd9b8cdb75fc401
iskonplus <58971909+iskonplus@users.noreply.github.com>
2021-08-11 13:48:01 +0300

    init: add save searh value in localStorage

--- script.js
+++ script.js
@@ -5,44 +5,60 @@ const wrapperButtonNav = document.querySelector(".wrapper_button_nav");
 const buttonNav = document.querySelector(".button_nav");
 const containerNav = document.querySelector(".container.nav");
 const ulNav = document.querySelector(".nav.ul");
+const search = document.querySelector(".search");
+const searchButton = document.querySelector(".icon_search");
 
-console.log(blockHelp);
-like.forEach(el => { el.addEventListener("click", () => giveLike(el)) });
+// console.log(blockHelp);
+like.forEach((el) => {
+  el.addEventListener("click", () => giveLike(el));
+});
 
 function giveLike(el) {
-    el.classList.toggle("active");
+  el.classList.toggle("active");
 }
 
-windowElectricity.addEventListener("mouseover", () => openClousWindowElectricity());
-windowElectricity.addEventListener("mouseout", () => openClousWindowElectricity());
+windowElectricity.addEventListener("mouseover", () =>
+  openClousWindowElectricity()
+);
+windowElectricity.addEventListener("mouseout", () =>
+  openClousWindowElectricity()
+);
 
 function openClousWindowElectricity() {
-    blockHelp.classList.toggle("active");
+  blockHelp.classList.toggle("active");
 }
 
 wrapperButtonNav.addEventListener("click", () => openClousMenu());
 containerNav.addEventListener("click", () => openClousMenu());
 
-
 function openClousMenu() {
-    buttonNav.classList.toggle("active");
-    wrapperButtonNav.classList.toggle("active");
-    containerNav.classList.toggle("active");
-    ulNav.classList.toggle("active");
-    
+  buttonNav.classList.toggle("active");
+  wrapperButtonNav.classList.toggle("active");
+  containerNav.classList.toggle("active");
+  ulNav.classList.toggle("active");
 }
 
+// ------------------search----------------
 
-// 
+searchButton.addEventListener("click", () => getSearchValue());
+search.addEventListener("keydown", (e) => getSearchValue(e));
 
-// console.log(like)
 
-// contentServise.forEach((el, ind) => el.addEventListener("click", ()=> toggleBlockServise(el, ind)));
+console.log(localStorage.getItem ("searchValue"))
 
-// function toggleBlockServise(el, ind) {
-//   contentServise.forEach(elem => elem.classList.remove("active"));
-//   el.classList.add("active");
+const searchValue = !localStorage.getItem("searchValue") ?
+    [] :
+    JSON.parse(localStorage.getItem("searchValue"));
 
-//   blockPService.forEach(elem => elem.classList.remove("active"));
-//   blockPService[ind].classList.add("active");
-// }
+function getSearchValue(e) {
+  !!e ?
+      e.key === "Enter" &&  setInLocalStorage():
+      setInLocalStorage();
+}
+
+function setInLocalStorage() {
+  searchValue.push(search.value);
+  localStorage.setItem("searchValue", JSON.stringify(searchValue));
+    search.value = "";
+    // localStorage.clear()
+}
