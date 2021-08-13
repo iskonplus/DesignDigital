a34bbb50df4dff149b518d648d5d3ef6c45fa02b
iskonplus <58971909+iskonplus@users.noreply.github.com>
2021-08-13 14:37:30 +0300

    init: save like in localstorage

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const like = document.querySelectorAll(".like");
+// const like = document.querySelectorAll(".like");
 const windowElectricity = document.querySelector(".container.subscribe>p");
 const blockHelp = document.querySelector(".wraper_block_help");
 const wrapperButtonNav = document.querySelector(".wrapper_button_nav");
@@ -10,13 +10,6 @@ const searchButton = document.querySelector(".icon_search");
 const formSubscribe = document.querySelector(".form_subscribe");
 
 // console.log(blockHelp);
-like.forEach((el) => {
-  el.addEventListener("click", () => giveLike(el));
-});
-
-function giveLike(el) {
-  el.classList.toggle("active");
-}
 
 windowElectricity.addEventListener("mouseover", () =>
   openClousWindowElectricity()
@@ -68,7 +61,9 @@ let chechEmail;
 const inputEmail = formSubscribe.elements.email;
 const inputSubmit = formSubscribe.elements.submit;
 
-formSubscribe.elements.submit.addEventListener("click", (event) => getValueSubscribe(event));
+formSubscribe.elements.submit.addEventListener("click", (event) =>
+  getValueSubscribe(event)
+);
 
 function getValueSubscribe(event) {
   event.preventDefault();
@@ -78,13 +73,14 @@ function getValueSubscribe(event) {
     inputEmail.value = "";
     inputEmail.style.border = "1px solid #d6e6f5";
     inputSubmit.style.border = "1px solid #d6e6f5";
-
+    inputEmail.style.borderRight = "none";
+    inputSubmit.style.borderLeft = "none";
   }
 }
 
 inputEmail.addEventListener("input", () => validationEmail());
 
-function validationEmail () {
+function validationEmail() {
   const reg = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
   const address = inputEmail.value;
 
@@ -101,7 +97,32 @@ function validationEmail () {
     inputEmail.style.borderRight = "none";
     inputSubmit.style.border = "1px solid green";
     inputSubmit.style.borderLeft = "none";
-
   }
 }
+
+// -------------get like----------------
+
+const likeHtml = document.querySelectorAll(".like");
+
+let islike = !localStorage.getItem("Like")
+  ? []
+  : JSON.parse(localStorage.getItem("Like"));
+
+
+islike && islike.forEach((el, ind) => likeHtml[ind].classList = `${el}`);
+  
+
+likeHtml.forEach((el) => {
+  el.addEventListener("click", () => giveLike(el));
+});
+
+function giveLike(el) {
+  el.classList.toggle("active");
+  islike.length = 0;
+
+  likeHtml.forEach((el) => islike.push(el.className));
+  localStorage.removeItem("Like");
+  localStorage.setItem("Like", JSON.stringify(islike));
+}
+
 // localStorage.clear();
